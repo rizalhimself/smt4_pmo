9338aad8fa6def0443ba10065303e6153f22b022
rizalhimself <sniperinstink@hotmail.com>
2021-03-23 10:24:57 +0700

    Diupload pada tanggal 2021-03-23 10:24:57

--- Latihan3/app/src/main/java/com/rizalhimself/latihan3/MainActivity.java
+++ Latihan3/app/src/main/java/com/rizalhimself/latihan3/MainActivity.java
@@ -1,8 +1,11 @@
 package com.rizalhimself.latihan3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView view = findViewById(R.id.tvCobaHTML);
+        String formattedText = "Ini adalah <i>sebuah</i> <b>percobaan</b>" +
+                " dari <a href='http://rizalhimself.blogspot.com'>Website Saya</a>";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            view.setText(Html.fromHtml(formattedText, Html.FROM_HTML_MODE_LEGACY));
+
+        } else {
+            view.setText(Html.fromHtml(formattedText));
+        }
+
     }
 }
