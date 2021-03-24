9613dfa4e4b360340203fd9926e71487feb78f46
rizalhimself <sniperinstink@hotmail.com>
2021-03-24 22:47:25 +0700

    Diupload pada tanggal 2021-03-24 22:47:25

--- Latihan3/app/src/main/java/com/rizalhimself/latihan3/MainActivity.java
+++ Latihan3/app/src/main/java/com/rizalhimself/latihan3/MainActivity.java
@@ -1,9 +1,15 @@
 package com.rizalhimself.latihan3;
 
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*TextView coba HTML*/
         TextView view = findViewById(R.id.tvCobaHTML);
         String formattedText = "<h1>TextView HTML</h1> Ini adalah <i>sebuah</i> <b>percobaan</b>" +
                 " <a href='http://rizalhimself.blogspot.com'>Website Saya</a>";
@@ -23,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
             view.setText(Html.fromHtml(formattedText));
         }
 
+        /*TextView coba HTML Panjang*/
         TextView tampilan2 = findViewById(R.id.tvCobaHTMLPanjang);
         String contohHTTPPanjang = getString(R.string.contohHttpPanjang);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
@@ -31,5 +39,33 @@ public class MainActivity extends AppCompatActivity {
             tampilan2.setText(Html.fromHtml(contohHTTPPanjang));
         }
 
+        /*TextView Ime Options Kirim*/
+        EditText etKirimin = (EditText) findViewById(R.id.etKirim);
+        etKirimin.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND){
+                    Toast.makeText(MainActivity.this,
+                            "Proses Kirim", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
+        /*TextView Ime Options Cari*/
+        EditText etCariin = (EditText) findViewById(R.id.etCari);
+        etCariin.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                if (actionId == EditorInfo.IME_ACTION_SEARCH){
+                    Toast.makeText(MainActivity.this, "Sedang Dicari Um", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
+        /*TextView Suggestion*/
+        
     }
 }
