b0b22247e16cea6af36cae423c6da8b40a1da0f7
rizalhimself <sniperinstink@hotmail.com>
2021-03-24 12:28:51 +0700

    Diupload pada tanggal 2021-03-24 12:28:51

--- Latihan3/app/src/main/java/com/rizalhimself/latihan3/MainActivity.java
+++ Latihan3/app/src/main/java/com/rizalhimself/latihan3/MainActivity.java
@@ -15,13 +15,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView view = findViewById(R.id.tvCobaHTML);
-        String formattedText = "Ini adalah <i>sebuah</i> <b>percobaan</b>" +
-                " dari <a href='http://rizalhimself.blogspot.com'>Website Saya</a>";
+        String formattedText = "<h1>TextView HTML</h1> Ini adalah <i>sebuah</i> <b>percobaan</b>" +
+                " <a href='http://rizalhimself.blogspot.com'>Website Saya</a>";
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             view.setText(Html.fromHtml(formattedText, Html.FROM_HTML_MODE_LEGACY));
-
         } else {
             view.setText(Html.fromHtml(formattedText));
+        }
+
+        TextView tampilan2 = findViewById(R.id.tvCobaHTMLPanjang);
+        String contohHTTPPanjang = getString(R.string.contohHttpPanjang);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            tampilan2.setText(Html.fromHtml(contohHTTPPanjang, Html.FROM_HTML_MODE_LEGACY));
+        } else {
+            tampilan2.setText(Html.fromHtml(contohHTTPPanjang));
         }
 
     }
