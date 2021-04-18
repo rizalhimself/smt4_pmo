30790756a108bc3d557322ecda07f63ce98d10a2
rizalhimself <sniperinstink@hotmail.com>
2021-04-18 12:54:27 +0700

    Diupload pada tanggal 2021-04-18 12:54:27

---
+++ Latihan6/app/src/main/java/com/rizalhimself/latihan6/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.rizalhimself.latihan6;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
